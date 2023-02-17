22601c0199f5aeca53a15d0f8fdd325b80d1925c
kubat <kubat.aiupov@gmail.com>
2023-02-17 06:19:41 +0100

    init justCoolStuff

---
+++ justCoolStuff.js
@@ -0,0 +1,36 @@
+// Write a function justCoolStuff() that takes in two arrays of strings, and,
+// using the built-in .filter() method, returns an array with the items
+// that are present in both arrays.
+
+// Write your code here:
+
+const justCoolStuff = (arr1, arr2) => {
+  return arr1.filter((item) => arr2.indexOf(item) > -1);
+};
+
+// Feel free to uncomment the code below to test your function
+
+const coolStuff = [
+  "gameboys",
+  "skateboards",
+  "backwards hats",
+  "fruit-by-the-foot",
+  "pogs",
+  "my room",
+  "temporary tattoos",
+];
+
+const myStuff = [
+  "rules",
+  "fruit-by-the-foot",
+  "wedgies",
+  "sweaters",
+  "skateboards",
+  "family-night",
+  "my room",
+  "braces",
+  "the information superhighway",
+];
+
+console.log(justCoolStuff(myStuff, coolStuff));
+// Should print [ 'fruit-by-the-foot', 'skateboards', 'my room' ]
